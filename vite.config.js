cc67d860cb0aa89c8f20149738c3e43bb1f32152
martsrodrigues <diversosmsr.2022@gmail.com>
2025-09-21 02:01:04 +0100

    fix: adiciona plugin React para Vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3002,    // escolhe uma porta fixa que raramente será usada
-    strictPort: true  // se a porta estiver ocupada, o Vite falha em vez de mudar
-  }
 });
+
