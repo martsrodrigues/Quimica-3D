124a98b3e56b5bffcf46bef05ac2fca49d9d38d2
martsrodrigues <diversosmsr.2022@gmail.com>
2025-09-21 12:02:51 +0100

    Corrigido vite.config.js para Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuração para Netlify/GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/', // em Netlify pode ser '/', em GitHub Pages teria de ser './'
+  base: './'
 })
+
 
 
