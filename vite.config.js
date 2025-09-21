c5cb088c11d554e4f2f83e888a0ac70a4b657f72
martsrodrigues <diversosmsr.2022@gmail.com>
2025-09-21 11:52:36 +0100

    Primeira versão

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Configuração para Netlify/GitHub Pages
 export default defineConfig({
   plugins: [react()],
-});
+  base: '/', // em Netlify pode ser '/', em GitHub Pages teria de ser './'
+})
+
 
